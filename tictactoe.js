869579ae763a50ba4e06f9a4a65670a62fe89949
Ulrics <66984492+Ulrics@users.noreply.github.com>
2025-07-09 15:41:19 -0700

    refactored playRound, startGame, gameBoard into module IIFE

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')();
+
 function createPlayer (symbol) {
     this.symbol = symbol;
     let score = 0;
@@ -7,16 +9,15 @@ function createPlayer (symbol) {
     return { symbol, getScore, addScore, resetScore };
 }
 
-const createCoords = (coordinates, occupied) => {
+const gameBoard = (function () {
+    let gameArray = [];
+
+    const createCoords = (coordinates, occupied) => {
     return {
         coordinates: coordinates,
         occupied: occupied
+        }
     }
-}
-
-function createGameBoard () {
-    let gameArray = [];
-
     const setUpGame = () => {
         for (let i = 0; i < 9; i++){
             gameArray.push(createCoords(i, null));
@@ -24,29 +25,120 @@ function createGameBoard () {
     }
     const getGame = () => gameArray;
     const resetGame = () => gameArray = [];
-    return { setUpGame, getGame, resetGame };
-}
 
-function checkWin(Array){
-    const checkHorizontal = () => {
-        const horiz1 = [0, 1, 2];
-        const horiz2 = [3, 4, 5];
-        const horiz3 = [6, 7, 8];
-        const horizCombos = [horiz1, horiz2, horiz3]
-        console.log(horizCombos[0][2])
-        for (let i = 0; i < horizCombos.length(); i++){
-            for (let y = 0; y < horizCombos[i].length(); y++){
-
+    const checkWin = () => {
+        const combos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], 
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        for (let i = 0; i < combos.length; i++){
+            const a = gameArray[combos[i][0]].occupied;
+            const b = gameArray[combos[i][1]].occupied;
+            const c = gameArray[combos[i][2]].occupied;
+            if (a !== null && a === b && a === c){
+                return { won: true, symbol: a };
             }
         }
+        return { won: false, symbol: null };
     }
-}
 
-const horiz1 = [0, 1, 2];
-const horiz2 = [3, 4, 5];
-const horiz3 = [6, 7, 8];
-const horizCombos = [horiz1, horiz2, horiz3]
-console.log(horizCombos[0][2])
+    const displayTerminal = () => {
+    let display = "";
+        for (let i = 0; i < 9; i += 3) {
+            const row = gameArray.slice(i, i + 3)
+                .map(cell => cell.occupied === null ? " " : cell.occupied)
+                .join(" | ");
+            display += row + "\n";
+            if (i < 6) display += "---------\n";
+        }
+    console.log(display);
+    };
+
+    return { setUpGame, getGame, resetGame, checkWin, displayTerminal };
+})();
+
+const startGame = (function () {
+    let player1;
+    let player2;
+
+    const init = () =>{
+        let playerChoice = prompt("Choose X or O", "X");
+        if(playerChoice === "x" || playerChoice === "o"){
+            playerChoice = playerChoice.toUpperCase(); 
+        }
+        if(playerChoice !== "X" && playerChoice !== "O"){
+            playerChoice = "X";
+            console.log("Invalid choice. Defaulting to X.");
+        }
+        player1 = createPlayer(playerChoice);
+        console.log(`You chose to play as: ${playerChoice}`);
+        if(player1.symbol === "X"){
+            player2 = createPlayer("O");
+        }
+        else{
+            player2 = createPlayer("X");
+        }
+    }
+    const getPlayers = () => ({ player1, player2 });
+
+    return { init, getPlayers };
+})();
+
+const playRound = (function (){
+    startGame.init();
+    const players = startGame.getPlayers()
+    let currentPlayer = players.player1;
+
+    const board = gameBoard;
+    board.setUpGame();
+
+    const switchPlayers = () =>{
+        if(currentPlayer === players.player1){
+            currentPlayer = players.player2;
+        }
+        else{
+            currentPlayer = players.player1;
+        }
+    }
+
+    const isOccupied = (coordinate) =>{
+        if(board.getGame()[coordinate].occupied !== null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    const placeMove = () =>{
+        board.displayTerminal();
+        let playerMove = prompt(`Choose where to place your move Player:${currentPlayer.symbol}`, "");
+        if(playerMove > 9){
+            placeMove();
+        }
+        if(isOccupied(playerMove) === true){
+            placeMove();
+        }
+        board.getGame()[playerMove].occupied = currentPlayer.symbol;
+    }
+
+    let turns = 0;
+    while(turns < 10){
+        placeMove(currentPlayer);
+        if(board.checkWin().won === true){
+            console.log(`${currentPlayer.symbol}'s won!`)
+            board.displayTerminal();
+            currentPlayer.addScore();
+
+            return currentPlayer;
+        }
+        switchPlayers();
+        turns++; 
+    }
+})();
+
+const gameController = (function (){
+
+})();
+
 /*
 const game1 = createGameBoard();
 game1.setUpGame();
